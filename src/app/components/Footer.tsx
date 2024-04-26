d76071cac5d6560bc5bf09465a7203831a497d4e
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2024-04-27 04:51:39 +0800

    [UPDATE] change footer color

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#274029] text-white py-4 text-center">
+    <footer className="bg-[#3B2D18] text-white py-4 text-center">
       <div className="container mx-auto">
         <p className="text-sm">&copy; 2024 Academia Quest. All rights reserved.</p>
         <p className="text-sm mt-2">Terms of Service | Privacy Policy | Cookie Policy</p>
